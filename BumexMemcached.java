9c9ed20ec2a39101b1c66519f9dc99085715d03e
mansillafederico <48660757+mansillafederico@users.noreply.github.com>
2019-03-17 21:02:47 -0300

    Ejercicio 1

---
+++ BumexMemcached.java
@@ -0,0 +1,27 @@
+
+public class BumexMemcached {
+
+	
+		private static final BumexMemcached instance = new BumexMemcached();
+		
+		private BumexMemcached() {
+			
+		}
+		
+		public static BumexMemcached getInstance() {
+			return instance;
+		}
+		
+		public void set(String key, Object value) {
+				
+		}
+		
+		public Object get(String key) {
+		
+			return null;
+		}
+		
+		public void delete(String key) {
+			
+	}
+}
---
+++ DAO/PedidosDAO.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import Model.Pedido;
+
+public class PedidosDAO {
+	
+	
+	public static void insertOrUpdate(Pedido pedido) {
+		
+	}
+	
+	public static void delete(Pedido pedido) {
+		
+	}
+	
+	public static Pedido select(int idPedido) {
+		return null;
+	}
+
+}
